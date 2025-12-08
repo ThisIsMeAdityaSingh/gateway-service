ed43d7a8a170d889ac11998a8202b3312e2ed2cf
ThisIsMeAdityaSingh <thisismeadityasingh99@gmail.com>
2025-12-08 11:32:24 +0530

    fix: I made a serious mistake

--- functions/src/utility/generate-error-log-payload.ts
+++ functions/src/utility/generate-error-log-payload.ts
@@ -3,7 +3,7 @@ import { ErrorLevels, ErrorSource, ServiceErrorTypes } from "../logging-service"
 export function generateErrorLogPayload(level: ErrorLevels, message: string, metadata: string|object, errorType: ServiceErrorTypes) {
     return {
         level,
-        source: ErrorSource.CLOUDFARE_WORKER,
+        source: ErrorSource.FIREBASE_FUNCTION, // since this function would only be used by gateway-service code
         message,
         requestId: `cloudfare_${crypto.randomUUID()}`,
         metadata,
